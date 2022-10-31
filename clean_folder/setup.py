d59c233e7d4329edea8fc725091c9796997dc091
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-10-31 16:20:05 +0200

    Create setup.py

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='clean_folder',
+      version='1.0',
+      description='Code for sort folder',
+      author='Mephodiy',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+            'console_scripts': ['clean_folder = clean_folder.clean:main'],
+},
+)
